27d860e32c60818571c4d781a95f1d522d128fe8
Anna Surahatava <anika3160@gmail.com>
2025-05-07 21:18:23 +0200

    feat: add update and delete fn

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,12 @@ import { isValidName, isValidAge, isValidHobbies } from './validators.js';
 
 export const getUserById = (id, users) => users.find(user => user.id === id);
 
-export const createUser = (username, age, hobbies) => {
-    if (isValidName(username) && isValidAge(age) && isValidHobbies(hobbies)) {
-        return ({ id: uuidv4(), username, age, hobbies })
+export const createOrUpdateUser = (username, age, hobbies, id) => {
+    if ((!id || uuidValidate(id)) 
+        && isValidName(username)
+        && isValidAge(age)
+        && isValidHobbies(hobbies)) {
+        return ({ id: id ? id : uuidv4(), username, age, hobbies })
     }
     throw new Error('Incorrect data or empty field. Please try again with correct data.')
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import http from 'http';
 import * as dotenv from 'dotenv';
 import { validate as uuidValidate } from 'uuid';
-import { getUserById, createUser } from './database/index.js';
+import { getUserById, createOrUpdateUser } from './database/index.js';
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-const users = [];
+let users = [];
 
 const server = http.createServer((req, res) => {
     console.log(`Server request ${req.url} ${req.method}`)
 
     const routesEl = req.url.split('/');
-    // console.log(routesEl);
 
     if (req.url === '/api/users') {
         switch(req.method) {
@@ -31,7 +30,7 @@ const server = http.createServer((req, res) => {
                 req.on('end', () => {
                     try {
                         const dataFromReq = JSON.parse(data);
-                        const newUser = createUser(dataFromReq.name, dataFromReq.age, dataFromReq.hobbies);
+                        const newUser = createOrUpdateUser(dataFromReq.name, dataFromReq.age, dataFromReq.hobbies);
                         users.push(newUser);
                         res.writeHead(201, {
                             'Content-Type': 'text/json'
@@ -55,33 +54,70 @@ const server = http.createServer((req, res) => {
         const isValidId = uuidValidate(routesEl[3]);
         const currentUser = getUserById(routesEl[3], users);
 
-        switch(req.method) {
-            case 'GET': {
-                if (isValidId) {
-                    if (currentUser) {
+        if (isValidId) {
+            if (currentUser) {
+                switch(req.method) {
+                    case 'GET': {
                         res.writeHead(200, {
                             'Content-Type': 'text/json'
                         });
                         res.write(JSON.stringify(currentUser))
                         res.end();
-                    } else {
-                        res.writeHead(404, {
+                        break;
+                    }
+                    case 'PUT': {
+                        let data = '';
+                        req.on('data', chunk => {
+                            data += chunk;
+                        })
+                        req.on('end', () => {
+                            try {
+                                const dataFromReq = JSON.parse(data);
+                                const newUserData = createOrUpdateUser(dataFromReq.name, 
+                                    dataFromReq.age,
+                                    dataFromReq.hobbies,
+                                    routesEl[3]);
+                                users = users.filter(user => user.id !== routesEl[3]);
+                                users.push(newUserData);
+                                res.writeHead(200, {
+                                    'Content-Type': 'text/json'
+                                });
+                                res.write(JSON.stringify(newUserData))
+                                res.end();
+                            } catch (error) {
+                                res.writeHead(400, {
+                                    'Content-Type': 'text/html'
+                                });
+                                res.write(error.message)
+                                res.end();
+                            }
+                        })
+                        break;
+                    }
+                    case 'DELETE': {
+                        users = users.filter(user => user.id !== routesEl[3]);
+                        res.writeHead(204, {
                             'Content-Type': 'text/html'
                         });
-                        res.write(`Record with ${routesEl} === userID doesn't exist`)
                         res.end();
-                        }
-                    } else {
-                        res.writeHead(400, {
-                            'Content-Type': 'text/html'
-                        });
-                        res.write('User id is invalid (not uuid)')
-                        res.end();
+                        break;
+                    }
+                    default: 
+                        break;
                 }
-                break;
+            } else {
+                res.writeHead(404, {
+                    'Content-Type': 'text/html'
+                });
+                res.write(`Record with ${routesEl[3]} === userID doesn't exist`)
+                res.end();
             }
-            default: 
-                break;
+        } else {
+            res.writeHead(400, {
+                'Content-Type': 'text/html'
+            });
+            res.write('User id is invalid (not uuid)')
+            res.end();
         }
     } else {
         res.writeHead(404, {
