b1163af21ec671032f83fe3b08f2f905a5b47209
Anna Surahatava <anika3160@gmail.com>
2025-05-07 21:13:14 +0200

    feat: add api users post method

--- src/index.js
+++ src/index.js
@@ -4,70 +4,83 @@ import { validate as uuidValidate } from 'uuid';
 import { getUserById, createUser } from './db.js';
 
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-
 const users = [];
-users.push(createUser('Anna', 26, ['ski', 'codding']));
-
 
 const server = http.createServer((req, res) => {
     console.log(`Server request ${req.url} ${req.method}`)
 
     const routesEl = req.url.split('/');
-    console.log(routesEl);
+    // console.log(routesEl);
 
-// get user by id
-   if (routesEl.length === 4
-        && routesEl[1] === 'api'
-        && routesEl[2] === 'users'
-        && req.method === 'GET') {
-            const isValidId = uuidValidate(routesEl[3]);
-            const currentUser = getUserById(routesEl[3], users);
-
-            if (isValidId) {
-                if (currentUser) {
-                    res.writeHead(200, {
-                        'Content-Type': 'text/json'
-                    });
-                    res.write(JSON.stringify(currentUser))
-                    res.end();
-                } else {
-                    res.writeHead(404, {
-                        'Content-Type': 'text/html'
-                    });
-                    res.write(`Record with ${routesEl} === userID doesn't exist`)
-                    res.end();
-                }
-            } else {
-                res.writeHead(400, {
-                    'Content-Type': 'text/html'
-                });
-                res.write('User id is invalid (not uuid)')
-                res.end();
-            }
-        }
-    
-    switch(req.url) {
-        case '/':
-            break;
-        case '/api/users': {
-            if (req.method === 'GET') {
+    if (req.url === '/api/users') {
+        switch(req.method) {
+            case 'GET': {
                 res.writeHead(200, {
                     'Content-Type': 'text/json'
                 });
                 res.write(JSON.stringify(users))
                 res.end();
+                break;
             }
-            break;
+            case 'POST': {
+                let data = '';
+                req.on('data', chunk => {
+                    data += chunk;
+                })
+                req.on('end', () => {
+                    const dataFromReq = JSON.parse(data);
+                    const newUser = createUser(dataFromReq.name, dataFromReq.age, dataFromReq.hobbies);
+                    users.push(newUser);
+                    res.writeHead(201, {
+                        'Content-Type': 'text/json'
+                    });
+                    res.write(JSON.stringify(newUser))
+                    res.end();
+                })
+                    break;
+                }
+                default:
+                    break;
         }
-        default:
-            // res.writeHead(404, {
-            //     'Content-Type': 'text/html'
-            // });
-            // res.write('Non defined')
-            // res.end();
-            break;
+    } else if (req.url.indexOf('/api/users/') === 0 && routesEl.length === 4) {
+        const isValidId = uuidValidate(routesEl[3]);
+        const currentUser = getUserById(routesEl[3], users);
+
+        switch(req.method) {
+            case 'GET': {
+                if (isValidId) {
+                    if (currentUser) {
+                        res.writeHead(200, {
+                            'Content-Type': 'text/json'
+                        });
+                        res.write(JSON.stringify(currentUser))
+                        res.end();
+                    } else {
+                        res.writeHead(404, {
+                            'Content-Type': 'text/html'
+                        });
+                        res.write(`Record with ${routesEl} === userID doesn't exist`)
+                        res.end();
+                        }
+                    } else {
+                        res.writeHead(400, {
+                            'Content-Type': 'text/html'
+                        });
+                        res.write('User id is invalid (not uuid)')
+                        res.end();
+                }
+                break;
+            }
+            default: 
+                break;
+        }
+    } else {
+        res.writeHead(404, {
+            'Content-Type': 'text/html'
+        });
+        res.write('Unknown URL')
+        res.end();
     }
 })
 
